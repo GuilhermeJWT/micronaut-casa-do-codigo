95781b1b3413d78621eaba40fcc21b5c714e00ec
GuilhermeJWT <guiromanno@gmail.com>
2021-06-05 12:12:27 -0300

    Mapeando queries explicitamente no Micronaut Data

--- src/main/kotlin/br/com/zup/controller/CadastroAutorController.kt
+++ src/main/kotlin/br/com/zup/controller/CadastroAutorController.kt
@@ -44,7 +44,7 @@ class CadastroAutorController(val autorRepository: AutorRepository) {
             return HttpResponse.ok(resposta)
         }
 
-        val possivelAutor = autorRepository.findByEmail(email)
+        val possivelAutor = autorRepository.buscaPorEmail(email)
 
         if(possivelAutor.isEmpty){
             return HttpResponse.notFound()
--- src/main/kotlin/br/com/zup/repository/AutorRepository.kt
+++ src/main/kotlin/br/com/zup/repository/AutorRepository.kt
@@ -1,6 +1,7 @@
 package br.com.zup.repository
 
 import br.com.zup.model.ModelAutor
+import io.micronaut.data.annotation.Query
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.jpa.repository.JpaRepository
 import java.util.*
@@ -9,5 +10,8 @@ import java.util.*
 interface AutorRepository : JpaRepository<ModelAutor, Long> {
 
     fun findByEmail(email: String) : Optional<ModelAutor>
+
+    @Query("SELECT a FROM ModelAutor a WHERE a.email = :email")
+    fun buscaPorEmail(email: String): Optional<ModelAutor>
 
 }
